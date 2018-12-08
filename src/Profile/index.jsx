270a8af3632c1a600bc783cd2a5d76b10eb159ff
tmstani23 <tmstani23@gmail.com>
2018-12-08 11:55:24 -0600

    Update 3
    Added link component that displays a link to the repository's github page.  Added repository item component.  The component destructures the input data node into relavent user repository information like name, url, stargazers, description, etc and displays them in separate divs and headers.  Added RepositoryList component that loops through all the passed in repositories and creates a div for each one calling the repository item component within the div and displaying.  The node data object is passed in as a spread operator for destructuring.

--- src/Profile/index.jsx
+++ src/Profile/index.jsx
@@ -4,6 +4,8 @@ import { Query } from 'react-apollo';
 import Loading from '../Loading';
 import RepositoryList from '../Repository';
 
+//GQL query that defines a query string using gql tag to be passed into the apollo client.
+//Gets user repository information data from the github api.
 const GET_REPOSITORIES_OF_CURRENT_USER = gql`
     {
         viewer {
@@ -35,18 +37,19 @@ const GET_REPOSITORIES_OF_CURRENT_USER = gql`
         }
     }  
 `
-
+//Profile component used for defining the query and displaying repository component
 const Profile = () => (
+    // Query is an Apollo React component that executes a gql query string when rendered.
     <Query query={GET_REPOSITORIES_OF_CURRENT_USER}>
-        
+        {/* Display repository or loading component depending on returned data state */}
         {({ data, loading }) => {
-            console.log(data)
+            
             const { viewer } = data;
-
+            // If viewer data is not returned or loading state is true return loading component
             if (!viewer || loading) {
                 return <Loading />
             }
-
+            //Else display Repository list component passing down the viewer repository data.
             return <RepositoryList repositories={viewer.repositories} />
         }}
     </Query>
--- src/Repository/index.js
+++ src/Repository/index.js
@@ -1,11 +1,4 @@
-import React from 'react';
-import RepositoryItem from '../RepositoryItem';
-import '../style.css';
-    
-const RepositoryList = ({ repositories }) =>
-    repositories.edges.map(({ node }) => (
-        <div key={node.id} className="RepositoryItem">
-            <RepositoryItem {...node} />
-        </div>
-    ));
+import RepositoryList from './RepositoryList';
+
+
 export default RepositoryList;
